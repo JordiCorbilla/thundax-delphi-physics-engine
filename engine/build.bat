a6e4f52b2bd05c591bcfd2228cc5eb1b31a429c6
jordi.coll.corbilla@gmail.com <jordi.coll.corbilla@gmail.com@8cfab75c-7606-a766-ac88-80a524681206>
2012-06-11 19:21:04 +0000

    update missing project references

--- engine/build.bat
+++ engine/build.bat
@@ -5,8 +5,8 @@ rem ****************************************************************************
 call rsvars
 
 
-msbuild /v:q bpl\TDPhysicsEngine.dproj   /target:Clean  /p:config=Debug
-msbuild /v:q bpl\TDPhysicsEngine.dproj   /target:Build  /p:config=Debug
+msbuild /v:q Package\TDPhysics.Engine.dproj   /target:Clean  /p:config=Debug
+msbuild /v:q Package\TDPhysics.Engine.dproj   /target:Build  /p:config=Debug
 
 msbuild /v:q Projects\ThundaxBallsDemo\ThundaxBallsDemo.dproj   /target:Clean  /p:config=Debug
 msbuild /v:q Projects\ThundaxBallsDemo\ThundaxBallsDemo.dproj   /target:Build  /p:config=Debug
@@ -14,3 +14,13 @@ msbuild /v:q Projects\ThundaxBallsDemo\ThundaxBallsDemo.dproj   /target:Build  /
 msbuild /v:q Projects\ThundaxSimulationPlant\ThundaxSimulationPlant.dproj   /target:Clean  /p:config=Debug
 msbuild /v:q Projects\ThundaxSimulationPlant\ThundaxSimulationPlant.dproj   /target:Build  /p:config=Debug
 
+msbuild /v:q Projects\ThundaxJansenMechanism\ThundaxJansenMechanism.dproj   /target:Clean  /p:config=Debug
+msbuild /v:q Projects\ThundaxJansenMechanism\ThundaxJansenMechanism.dproj   /target:Build  /p:config=Debug
+
+msbuild /v:q Projects\ThundaxObjectsDemo\ThundaxObjectDemo.dproj   /target:Clean  /p:config=Debug
+msbuild /v:q Projects\ThundaxObjectsDemo\ThundaxObjectDemo.dproj   /target:Build  /p:config=Debug
+
+msbuild /v:q Projects\ThundaxTDPETests\TDPETests.dproj   /target:Clean  /p:config=Debug
+msbuild /v:q Projects\ThundaxTDPETests\TDPETests.dproj   /target:Build  /p:config=Debug
+
+pause
