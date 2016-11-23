b584d295d5561338bc2c01c14f24bf4ccf9f9490
JordiCorbilla <jordi.coll.corbilla@gmail.com>
2016-11-23 22:54:44 +0000

    Upgrade to delphi 10.1 berlin update 2

--- engine/Projects/ThundaxTDPEDemo/ufrmDemo.pas
+++ engine/Projects/ThundaxTDPEDemo/ufrmDemo.pas
@@ -1,3 +1,35 @@
+(*
+ * Copyright (c) 2010-2016 Thundax Delphi Physics Engine
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ *
+ * * Neither the name of 'TDPE' nor the names of its contributors
+ *   may be used to endorse or promote products derived from this software
+ *   without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *)
+
 unit ufrmDemo;
 
 interface
@@ -94,15 +126,15 @@ end;
 
 procedure TForm2.FormCreate(Sender: TObject);
 begin
-//  Engine := TFluentEngine.New(1 / 4).AddInitialForce(TForce.Create(false, 0, 2)).AddDamping(0).AddRestrictionCollitionCycles(10);
-//  Layout1.Canvas.BeginScene();
-//  Render := TFMXRenderer.Create(Layout1.Canvas);
-//  FGround := TScenery.Create(Render, Engine, clablue);
-//  Frobot := TRobot.Create(Render, Engine, 1050, 400, 1.3, 0.02);
-//  Engine.AddGroups(FGround).AddGroups(Frobot);
-//  FGround.AddCollidable(Frobot);
-//  Frobot.togglePower();
-//  Layout1.Canvas.EndScene;
+  Engine := TFluentEngine.New(1 / 4).AddInitialForce(TForce.Create(false, 0, 2)).AddDamping(0).AddRestrictionCollitionCycles(10);
+  Layout1.Canvas.BeginScene();
+  Render := TFMXRenderer.Create(Layout1.Canvas);
+  FGround := TScenery.Create(Render, Engine, clablue);
+  Frobot := TRobot.Create(Render, Engine, 1050, 400, 1.3, 0.02);
+  Engine.AddGroups(FGround).AddGroups(Frobot);
+  FGround.AddCollidable(Frobot);
+  Frobot.togglePower();
+  Layout1.Canvas.EndScene;
 
   //DoubleBuffered := true;
 end;
@@ -131,12 +163,12 @@ end;
 
 procedure TForm2.Timer1Timer(Sender: TObject);
 begin
-//  Engine.Run;
-//  Frobot.Run();
-//  Layout1.Canvas.BeginScene();
-//  Layout1.Canvas.Clear(claWhite);
-//  Engine.Paint;
-//  Layout1.Canvas.EndScene();
+  Engine.Run;
+  Frobot.Run();
+  Layout1.Canvas.BeginScene();
+  Layout1.Canvas.Clear(claWhite);
+  Engine.Paint;
+  Layout1.Canvas.EndScene();
 end;
 
 procedure TForm2.ToggleClick(Sender: TObject);
